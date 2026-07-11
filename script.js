6824bba3fc638b8051ff23de02c8f5ccb41719d2
NanoAI-26 <nanocontructai.26@gmail.com>
2026-07-11 10:31:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -574,3 +574,22 @@ console.log(
 "color:#00E5FF;font-size:18px;font-weight:bold"
 
 );
+/* ===========================
+   Navbar Scroll Effect
+=========================== */
+
+const nav = document.querySelector("nav");
+
+window.addEventListener("scroll", () => {
+
+    if (window.scrollY > 60) {
+
+        nav.classList.add("nav-scrolled");
+
+    } else {
+
+        nav.classList.remove("nav-scrolled");
+
+    }
+
+});
